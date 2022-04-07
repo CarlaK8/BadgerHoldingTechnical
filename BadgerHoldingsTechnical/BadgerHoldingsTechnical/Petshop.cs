0b256428e329ce0d12d6e0933a61283e6f9a8fd9
CarlaK8 <80395960+CarlaK8@users.noreply.github.com>
2022-04-07 13:05:53 +0200

    Add files via upload

---
+++ BadgerHoldingsTechnical/BadgerHoldingsTechnical/Petshop.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BadgerHoldingsTechnical
+{
+    static class Petshop
+    {
+    
+        public static double basePrice(int biscuits ,int chewies, int vitamins, string bucketSize)
+        {
+            double bPriceN = 2;
+            double bPriceL = 3.75;
+            double cPriceN = 4.10;
+            double cPriceL = 4.50;
+            double vPriceN = 2.50;
+            double vPriceL = 4.15;
+            double curPrice = 0.00;
+
+            if (bucketSize == "Normal")
+            {
+                curPrice =  (biscuits * bPriceN) + (chewies * cPriceN) + (vitamins * vPriceN);
+            } else
+            {
+                curPrice = (biscuits * bPriceL) + (chewies * cPriceL) + (vitamins * vPriceL);
+            }
+
+            return curPrice;
+        }
+        public static double applyWeekend(string isWeekend, string bucketSize,  double curPrice, int biscuits, int chewies, int vitamins)
+        {
+            double weekendPrice = 0.00;
+            double totalTreats = biscuits + chewies + vitamins;
+
+            if (isWeekend == "Y")
+            {
+                weekendPrice = curPrice + (curPrice * 0.15);
+            }
+            else
+            {
+                weekendPrice = curPrice;
+            }
+
+            if (totalTreats > 20)
+            {
+                weekendPrice = weekendPrice - (weekendPrice * 0.2);
+            }
+
+            if (vitamins > 7 && bucketSize == "Normal")
+            {
+                weekendPrice = weekendPrice - (weekendPrice * 0.05);
+            }
+
+            if (chewies >= 10 && bucketSize == "Large")
+            {
+                weekendPrice = weekendPrice - (weekendPrice * 0.1);
+            }
+
+            return weekendPrice;
+        }
+
+        
+
+
+
+    }
+    
+}
---
+++ BadgerHoldingsTechnical/BadgerHoldingsTechnical/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BadgerHoldingsTechnical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { try
+            {
+                Console.WriteLine("Please enter the number of biscuits");
+                int biscuits = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the number of chewies");
+                int chewies = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the number of vitamins");
+                int vitamins = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the bucket size as Large or Normal");
+                string bucketSize = Console.ReadLine();
+                Console.WriteLine("Is this over a weekend? Y/N");
+                string isWeekend = Console.ReadLine();
+
+                var basePrice = Petshop.basePrice(biscuits, chewies, vitamins, bucketSize);
+                var weekendPrice = (Petshop.applyWeekend(isWeekend, bucketSize, basePrice, biscuits, chewies, vitamins));
+                var bucketFillCharge = 2;
+
+                var finPrice = weekendPrice + bucketFillCharge;
+
+                Console.WriteLine( "The final price is :" + finPrice.ToString());
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+        }
+
+       
+    }
+
+}
+
